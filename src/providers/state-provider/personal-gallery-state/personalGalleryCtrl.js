b432ba26f4afa25cc30063fec4bc7becb5a8a384
Whitney Harvey <wmelrose01@gmail.com>
2016-03-10 14:40:25 -0800

    personal gallery page now loads logged in users images

--- src/providers/state-provider/personal-gallery-state/personalGalleryCtrl.js
+++ src/providers/state-provider/personal-gallery-state/personalGalleryCtrl.js
@@ -1,9 +1,8 @@
 'use strict';
 export default function(ngModule) {
-  ngModule.controller('personalGalleryCtrl',['$scope','loadPersonal', function($scope, loadPersonal) {
+  ngModule.controller('personalGalleryCtrl', function($scope, loadPersonal) {
     $scope.personal = {};
     $scope.personal.user = 'Batman';
-    $scope.personal.svgImages = loadPersonal.svgImages;
-  }]
-);
+    $scope.personal.svgImages = loadPersonal;
+  });
 }
