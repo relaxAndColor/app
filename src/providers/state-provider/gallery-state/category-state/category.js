756148587be249932b0f4077a512f168974e1939
Whitney Harvey <wmelrose01@gmail.com>
2016-03-11 11:24:37 -0800

    add svg files and fix popular sort

--- src/providers/state-provider/gallery-state/category-state/category.js
+++ src/providers/state-provider/gallery-state/category-state/category.js
@@ -10,7 +10,7 @@ export default {
   resolve: {
     svg (Gallery, $sce, $stateParams,$timeout) {
       window.scrollTo(0, 0);
-      var query = { sort: '-view' };
+      var query = { sort: '-views' };
       if ($stateParams.categoryName !== 'Popular') {
         query.category = $stateParams.categoryName;
       }
